c4db654703c31aae6cef739b17d49efdd8289c2b
Daniel <danielivanhola@gmail.com>
2019-09-02 00:07:41 -0500

    Loop bonito

---
+++ 2.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+daniel = turtle.Turtle()
+daniel.forward(50)
+daniel.left(90)
+daniel.forward(50)
+daniel.left(90)
+daniel.forward(50)
+daniel.left(90)
+daniel.forward(50)
+daniel.left(90)
+
+turtle.mainloop()
