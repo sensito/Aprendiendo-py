1d648a6a72f627499bfe8753c1c816b83c161c37
sensito <41710949+sensito@users.noreply.github.com>
2019-11-06 23:33:46 -0600

    Update aburrido.py
    Final del reto

--- aburrido.py
+++ aburrido.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
 def recive_word():
-    word = str(input('Dame tu parabra para revertit'))
+    word = str(input('Dame tu parabra para revertir:  '))
     return word
+
+def invert_words(word):
+    return word[::-1]
+
+
 
 if __name__ == '__main__':
     word = recive_word()
-    reverse_word = lambda word: word[::-1]
-    reverse_word_space= lambda  reverse_word: reverse_word.split(' ')
-    
+    reverse_word = invert_words(word)
+    print(reverse_word)
