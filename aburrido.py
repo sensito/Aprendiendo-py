75854071d1714bc78bfbdd8a66955ca20b4a6924
sensito <41710949+sensito@users.noreply.github.com>
2019-11-06 23:09:39 -0600

    Update aburrido.py
    oracticando funciones lambda

--- aburrido.py
+++ aburrido.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
-def run():
-    pass
-
+def recive_word():
+    word = str(input('Dame tu parabra para revertit'))
+    return word
 
 if __name__ == '__main__':
-    run()
+    word = recive_word()
+    reverse_word = lambda word: word[::-1]
+    reverse_word_space= lambda  reverse_word: reverse_word.split(' ')
+    
