4a1633047bbff5c244fbfb62349a7672959a944b
sensito <41710949+sensito@users.noreply.github.com>
2019-11-07 23:16:54 -0600

    Create word.py
    Practica de except

---
+++ reversed/word.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+def recive_word():
+    word = str(input('Dame tu parabra para revertir:  '))
+    return word
+
+def invert_words(word):
+    return word[::-1]
+
+if __name__ == '__main__':
+    word = recive_word()
+    reverse_word = invert_words(word)
+    print(reverse_word)
