307f0c4f763a698f8674bde412b0ab56c4b4cddf
sensito <41710949+sensito@users.noreply.github.com>
2019-11-09 19:25:00 -0600

    Create fundamentos_poo.py
    Inicio de fundamentos poo

---
+++ fundamentos_poo.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+class lampara(object):
+    """docstring for lampara."""
+
+    def __init__(self, arg):
+        super(lampara, self).__init__()
+        self.arg = arg
+
+
+
+def run():
+    pass
+
+if __name__ == '__main__':
+    run()
